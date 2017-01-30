802c750996df634c9b6df28967ff30b69a3fd5e2
Ivan_Surus <Ivan_Surus@emap.com>
2017-01-30 19:53:29 +0300

    add Controller class

---
+++ com/epam/catalog/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.epam.catalog.controller;
+
+/*данные на слое Controller будут приходить в виде форматированной строки.
+Ответ клиенту Контроллер также будет осуществлять в виде форматированной строки*/
+
+public class Controller {
+
+    public String executeTask(String request){
+        return null;//stub
+    }
+
+}
